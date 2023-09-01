0d2266b50ec089bc80440438a55c05c23061e3de
IslaMCastro <133907365+IslaMCastro@users.noreply.github.com>
2023-08-31 21:58:45 -0300

    Alterações

--- src/components/tipo/Tipo.jsx
+++ src/components/tipo/Tipo.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-
+import { useState } from "react";
 
 function Tipo() {
+    const [descricao, setDescricao] = useState("");
+    function criarObjeto(e){
+        setDescricao(e.target.value);
+    }
     return (
         <div>
             <div>
-                <h2>Formulário</h2>                
-                    <form className="form-floating">
-                        <input 
-                        type="text" 
-                        className="form-control" 
-                        id="descricao" 
-                        placeholder="ex: Reunião" 
-                        value="" />
-                        <label htmlFor="descricao">Digite o Tipo</label>
-                    </form>               
+                <h2>Formulário</h2>
+                <form className="form-floating">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="descricao"
+                        placeholder="ex: Reunião"
+                        value={descricao} 
+                        onChange={criarObjeto}
+                        />
+                    <label htmlFor="descricao">Digite o Tipo</label>
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button className="btn btn-primary me-md-2" type="button">salvar</button>
+                        <button className="btn btn-primary" type="button">deletar</button>
                     </div>
+                </form>
+            </div>
             <div>
                 <h1>Tabela</h1>
             </div>
