4765369ead5e77c35c66a5e7963c1b505ce67106
IslaMCastro <133907365+IslaMCastro@users.noreply.github.com>
2023-09-12 19:49:19 -0300

    Alterações

--- src/components/tipo/Tipo.jsx
+++ src/components/tipo/Tipo.jsx
@@ -3,30 +3,63 @@ import { useState } from "react";
 
 function Tipo() {
     const [descricao, setDescricao] = useState("");
-    
+    const [id, setId] = useState(0)
+    const [dados, setDados] = useState([])
+
+    const criar = (e) => {
+        e.preventDefault();
+        const index = id + 1;
+        const item = { index, descricao };
+        // o nome dos três pontos é spreds //
+        setDados([...dados, item]);
+        setId(index)
+        setDescricao('')
+    }
     return (
         <div>
             <div>
                 <h2>Formulário</h2>
-                <form className="form-floating">
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="descricao"
-                        placeholder="ex: Reunião"
-                        value={descricao}
-                        onChange={(e)=> setDescricao(e.target.value)}
-                    />
-                    <label htmlFor="descricao">Digite o Tipo</label>
-                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button className="btn btn-primary me-md-2" type="button">salvar</button>
-                        <button className="btn btn-primary" type="button">deletar</button>
+
+                <form className="" action="">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="descricao"
+                            placeholder="ex: Reunião"
+                            value={descricao}
+                            onChange={(e) => setDescricao(e.target.value)}
+                        />
+                        <label htmlFor="descricao">Digite o Tipo</label>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+
+                        </div>
+                        <button onClick={criar} className="btn btn-success">Salvar</button>
                     </div>
                 </form>
             </div>
             <div>
                 <h1>Tabela</h1>
             </div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dados.map((item,index) => (
+                        <tr key={index}>
+                            <td>{item.index}</td>
+                            <td>{item.descricao}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
         </div>
 
     );
---
+++ src/dados/TarefasDB.js
@@ -0,0 +1,10 @@
+const tarefas = [
+
+{id: 1, descricao : 'Ligar FABC', tipo: 'Pendente'},
+{id: 2, descricao : 'Ligar Senac', tipo: 'Realizado'},
+{id: 3, descricao : 'Ligar Deloitte', tipo: 'Agendado'},
+{id: 4, descricao : 'Ligar Kmm', tipo: 'Aguardado'},
+
+]
+
+export default tarefas
